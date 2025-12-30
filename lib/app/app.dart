f24f5f76bbe4657a88a2b44b377283ed230a3fc5
Majid Bhuiyan <majid.bhuiyan2021@gmail.com>
2025-12-30 22:42:55 +0600

    code setup

---
+++ lib/app/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CraftyBayApp extends StatefulWidget {
+  const CraftyBayApp({super.key});
+
+  @override
+  State<CraftyBayApp> createState() => _CraftyBeyAppState();
+}
+
+class _CraftyBeyAppState extends State<CraftyBayApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+      home: Scaffold(
+
+
+      ),
+    );
+  }
+}
