97caa58eff20761d268cd0180b5bacfb21c6c2b3
Majid Bhuiyan <majid.bhuiyan2021@gmail.com>
2026-01-16 15:21:58 +0600

    init firebase in this project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:crafty_bey/app/app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 
-void main(){
+import 'firebase_options.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(CraftyBayApp());
 }
