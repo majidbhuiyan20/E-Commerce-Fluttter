47b9882022d100b4c650166f973a42dc23c48b29
Majid Bhuiyan <majid.bhuiyan2021@gmail.com>
2026-02-08 01:07:45 +0600

    feat(ui): wishlist done

---
+++ lib/features/wish_list/presentation/screens/wish_list_screen.dart
@@ -0,0 +1,28 @@
+import 'package:crafty_bey/features/home/presentation/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class WishListScreen extends StatefulWidget {
+  const WishListScreen({super.key});
+
+  @override
+  State<WishListScreen> createState() => _WishListScreenState();
+}
+
+class _WishListScreenState extends State<WishListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Wishlist"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: GridView.builder(
+            itemCount: 30,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3, crossAxisSpacing: 8, mainAxisSpacing: 8) , itemBuilder: (context, index){
+          return FittedBox(child: ProductCard());
+        }),
+      ),
+    );
+  }
+}
