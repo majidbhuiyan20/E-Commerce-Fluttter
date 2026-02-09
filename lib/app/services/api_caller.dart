2dd361088ce7985d4ca02e7d85085c21fe7dad08
Majid Bhuiyan <majid.bhuiyan2011@gmail.com>
2026-02-09 12:03:53 +0600

    WIP(api): start api call with init api caller , logger

---
+++ lib/app/services/api_caller.dart
@@ -0,0 +1,142 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart';
+import 'package:logger/logger.dart';
+
+class ApiCaller {
+  static final Logger _logger = Logger();
+  static Future<ApiResponse> getRequest({required String url}) async {
+    try {
+      Uri uri = Uri.parse(url);
+      _logRequest(url);
+      Response response = await get(uri, headers: {
+
+      });
+
+      _logResponse(url, response);
+
+      final int statusCode = response.statusCode;
+
+      if (statusCode == 200 || statusCode == 201) {
+        final decodedData = json.decode(response.body);
+        return ApiResponse(
+          isSuccess: true,
+          responseCode: statusCode,
+          responseData: decodedData,
+        );
+      }
+      else if(statusCode == 401){
+        await _moveToLogin();
+        return ApiResponse(
+          isSuccess: false,
+          responseCode: statusCode,
+          responseData: null,
+          errorMessage: "Unauthorized",);
+      }
+      else {
+        final decodedData = json.decode(response.body);
+        return ApiResponse(
+          isSuccess: false,
+          responseCode: statusCode,
+          responseData: decodedData,
+        );
+      }
+    } on Exception catch (e) {
+      return ApiResponse(
+        isSuccess: false,
+        responseCode: -1,
+        responseData: null,
+        errorMessage: e.toString(),
+      );
+    }
+  }
+
+  static Future<ApiResponse> postRequest({
+    required String url,
+    Map<String, dynamic>? body,
+  }) async {
+    try {
+      Uri uri = Uri.parse(url);
+      _logRequest(url, body: body);
+      Response response = await post(
+          uri,
+          headers: {
+            'content-type': 'application/json',
+
+          },
+          body: jsonEncode(body));
+
+      _logResponse(url, response);
+
+      final int statusCode = response.statusCode;
+
+      if (statusCode == 200) {
+        final decodedData = json.decode(response.body);
+        return ApiResponse(
+          isSuccess: true,
+          responseCode: statusCode,
+          responseData: decodedData,
+        );
+      }
+      else if(statusCode == 401){
+        await _moveToLogin();
+        return ApiResponse(
+          isSuccess: false,
+          responseCode: statusCode,
+          responseData: null,
+          errorMessage: "Unauthorized",);
+      }
+      else {
+        final decodedData = json.decode(response.body);
+        return ApiResponse(
+          isSuccess: false,
+          responseCode: statusCode,
+          responseData: decodedData,
+        );
+      }
+    } on Exception catch (e) {
+      return ApiResponse(
+        isSuccess: false,
+        responseCode: -1,
+        responseData: null,
+        errorMessage: e.toString(),
+      );
+    }
+  }
+
+  static void _logRequest(String url, {Map<String, dynamic>? body}) {
+    _logger.i(
+      'URL => $url\n'
+      'Request Body => $body',
+    );
+  }
+
+  static void _logResponse(String url, Response response) {
+    _logger.i(
+      'URL => $url\n'
+      'Status Code => ${response.statusCode}\n'
+      'Body => ${response.body}',
+    );
+  }
+
+  static Future<void> _moveToLogin() async{
+    // await AuthController.clearUserData();
+    // Navigator.pushNamedAndRemoveUntil(TaskManagerApp.navigator.currentContext!, LoginScreen.name, (predicate)=> false);
+    //
+    //
+  }
+}
+
+class ApiResponse {
+  final bool isSuccess;
+  final int responseCode;
+  final dynamic responseData;
+  final String? errorMessage;
+
+  ApiResponse({
+    required this.isSuccess,
+    required this.responseCode,
+    required this.responseData,
+    this.errorMessage = "Something went wrong",
+  });
+}
