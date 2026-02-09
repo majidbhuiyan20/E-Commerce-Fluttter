d6a4c556b7015354a359acc2b3078ace797bb9c7
Majid Bhuiyan <majid.bhuiyan2011@gmail.com>
2026-02-09 10:42:26 +0600

    fix(ui)P: add pop scope in wishlist

--- lib/features/wish_list/presentation/screens/wish_list_screen.dart
+++ lib/features/wish_list/presentation/screens/wish_list_screen.dart
@@ -1,5 +1,7 @@
+import 'package:crafty_bey/features/common/presentation/provider/main_nav_container_provider.dart';
 import 'package:crafty_bey/features/home/presentation/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../../home/presentation/widgets/product_card.dart';
 
@@ -13,17 +15,23 @@ class WishListScreen extends StatefulWidget {
 class _WishListScreenState extends State<WishListScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Wishlist"),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: GridView.builder(
-            itemCount: 30,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3, crossAxisSpacing: 8, mainAxisSpacing: 8) , itemBuilder: (context, index){
-          return FittedBox(child: ProductCard());
-        }),
+    return PopScope(
+      onPopInvokedWithResult: (_, __){
+        context.read<MainNavContainerProvider>().changeToHome();
+      },
+      canPop: false,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Wishlist"),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: GridView.builder(
+              itemCount: 30,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3, crossAxisSpacing: 8, mainAxisSpacing: 8) , itemBuilder: (context, index){
+            return FittedBox(child: ProductCard());
+          }),
+        ),
       ),
     );
   }
