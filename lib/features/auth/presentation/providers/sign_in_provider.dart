2aaa7d8507177cebd685ee57485ef8d7be7f4fd2
Majid Bhuiyan <majid.bhuiyan2011@gmail.com>
2026-02-10 09:01:12 +0600

    feat(api): sign in api integration is completed

---
+++ lib/features/auth/presentation/providers/sign_in_provider.dart
@@ -0,0 +1,39 @@
+import 'package:crafty_bey/app/set_up_network_caller.dart';
+import 'package:crafty_bey/app/urls.dart';
+import 'package:crafty_bey/core/services/network_caller.dart';
+import 'package:crafty_bey/features/auth/data/models/sign_up_params.dart';
+import 'package:flutter/widgets.dart';
+
+import '../../data/models/sign_in_params.dart';
+
+class SignInProvider extends ChangeNotifier {
+  bool _isSignInInProgress = false;
+  bool get isSignUpInProgress => _isSignInInProgress;
+
+  String? _errorMessage;
+  String? get errorMessage => _errorMessage;
+
+
+  Future<bool> signIn(SignInParams params) async {
+    bool isSuccess = false;
+    _isSignInInProgress = true;
+    notifyListeners();
+
+    final NetworkResponse response = await getNetworkCaller().postRequest(
+      url: Urls.signInUrl,
+      body: params.toJson(),
+    );
+    if(response.isSuccess){
+      isSuccess = true;
+      _errorMessage = null;
+    }
+    else{
+      isSuccess = false;
+      _errorMessage = response.errorMessage;
+    }
+    _isSignInInProgress = false;
+    notifyListeners();
+
+    return isSuccess;
+  }
+}
--- lib/features/auth/presentation/screens/sign_in_screen.dart
+++ lib/features/auth/presentation/screens/sign_in_screen.dart
@@ -1,8 +1,14 @@
 import 'package:crafty_bey/app/app_colors.dart';
+import 'package:crafty_bey/features/auth/data/models/sign_in_params.dart';
+import 'package:crafty_bey/features/auth/presentation/providers/sign_in_provider.dart';
 import 'package:crafty_bey/features/auth/presentation/screens/sign_up_screen.dart';
 import 'package:crafty_bey/features/auth/presentation/widgts/app_logo.dart';
+import 'package:crafty_bey/features/common/presentation/screens/main_nav_holder_screen.dart';
+import 'package:crafty_bey/features/common/presentation/widgets/center_progress_indicator.dart';
+import 'package:crafty_bey/features/common/presentation/widgets/snack_bar_message.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SignInScreen extends StatefulWidget {
   const SignInScreen({super.key});
@@ -14,77 +20,107 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  final SignInProvider _signInProvider = SignInProvider();
+
+  final TextEditingController _emailTEController = TextEditingController();
+  final TextEditingController _passwordTEController = TextEditingController();
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     final textTheme = TextTheme.of(context);
-    return Scaffold(
-      resizeToAvoidBottomInset: true,
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: SingleChildScrollView(
-            child: Column(
-              spacing: 8,
-              children: [
-                AppLogo(width: 80),
-                Text(
-                  "Sign In",
-                  style: textTheme.headlineMedium?.copyWith(fontWeight: .bold),
-                ),
-                Text("Login to your account", style: textTheme.bodyLarge),
-                SizedBox(height: 16),
-                TextFormField(
-                  textInputAction: .next,
-                  keyboardType: .emailAddress,
-                  decoration: InputDecoration(hintText: "Email"),
-                  validator: (String? value) {
-                    if (value?.trim().isEmpty ?? true) {
-                      return "Enter Your Email";
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  obscureText: true,
-                  textInputAction: .next,
-                  obscuringCharacter: '*',
-                  decoration: InputDecoration(hintText: "Password"),
-                  validator: (String? value) {
-                    if (value?.trim().isEmpty ?? true) {
-                      return "Enter Your Password";
-                    }
-                    return null;
-                  },
-                ),
-                Row(
-                  mainAxisAlignment: .end,
+    return ChangeNotifierProvider(
+      create: (_)=> _signInProvider,
+      child: Scaffold(
+        resizeToAvoidBottomInset: true,
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: SingleChildScrollView(
+              child: Form(
+                key: _formKey,
+                autovalidateMode: .onUserInteraction,
+                child: Column(
+                  spacing: 8,
                   children: [
-                    TextButton(onPressed: _onTapForgotPassword, child: Text("Forgot Password"))
+                    AppLogo(width: 80),
+                    Text(
+                      "Sign In",
+                      style: textTheme.headlineMedium?.copyWith(
+                        fontWeight: .bold,
+                      ),
+                    ),
+                    Text("Login to your account", style: textTheme.bodyLarge),
+                    SizedBox(height: 16),
+                    TextFormField(
+                      controller: _emailTEController,
+                      textInputAction: .next,
+                      keyboardType: .emailAddress,
+                      decoration: InputDecoration(hintText: "Email"),
+                      validator: (String? value) {
+                        if (value?.trim().isEmpty ?? true) {
+                          return "Enter Your Email";
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      controller: _passwordTEController,
+                      obscureText: true,
+                      textInputAction: .next,
+                      obscuringCharacter: '*',
+                      decoration: InputDecoration(hintText: "Password"),
+                      validator: (String? value) {
+                        if (value?.trim().isEmpty ?? true) {
+                          return "Enter Your Password";
+                        }
+                        return null;
+                      },
+                    ),
+                    Row(
+                      mainAxisAlignment: .end,
+                      children: [
+                        TextButton(
+                          onPressed: _onTapForgotPassword,
+                          child: Text("Forgot Password"),
+                        ),
+                      ],
+                    ),
+                    Consumer<SignInProvider>(
+                      builder: (context, _, __) {
+                        return Visibility(
+                          visible: _signInProvider.isSignUpInProgress == false,
+                          replacement: CenterProgressIndicator(),
+                          child: FilledButton(
+                            onPressed: _onTapSignInButton,
+                            child: Text("Sign In"),
+                          ),
+                        );
+                      }
+                    ),
+                    SizedBox(height: 8),
+                    RichText(
+                      text: TextSpan(
+                        style: textTheme.bodyMedium,
+                        text: 'Don\'t have an account?  ',
+                        children: [
+                          TextSpan(
+                            style: TextStyle(
+                              color: AppColors.themeColor,
+                              fontWeight: .bold,
+                            ),
+
+                            text: 'Sign up',
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = _onTapSignUpButton,
+                          ),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
-                FilledButton(
-                  onPressed: _onTapSignInButton,
-                  child: Text("Sign In"),
-                ),
-                SizedBox(height: 8),
-                RichText(
-                  text: TextSpan(
-                    style: textTheme.bodyMedium,
-                    text: 'Don\'t have an account?  ',
-                    children: [
-                      TextSpan(
-                        style: TextStyle(
-                          color: AppColors.themeColor,
-                          fontWeight: .bold,
-                        ),
-
-                        text: 'Sign up',
-                        recognizer: TapGestureRecognizer()..onTap = _onTapSignUpButton,
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+              ),
             ),
           ),
         ),
@@ -92,12 +128,36 @@ class _SignInScreenState extends State<SignInScreen> {
     );
   }
 
-  void _onTapForgotPassword(){
-
-  }
+  void _onTapForgotPassword() {}
   void _onTapSignInButton() {
-
+    if (_formKey.currentState!.validate()) {
+      _signIn();
+    }
   }
+
+  Future<void> _signIn() async {
+    final bool isSuccess = await _signInProvider.signIn(
+      SignInParams(
+        email: _emailTEController.text.trim(),
+        password: _passwordTEController.text.trim(),
+      ),
+    );
+
+    if(isSuccess){
+      Navigator.pushNamedAndRemoveUntil(context, MainNavHolderScreen.routeName,(predicate) => false);
+      showSnackBarMessage(context, "SignIn Successful", backgroundColor: Colors.green);
+    }
+    else{
+      showSnackBarMessage(context, _signInProvider.errorMessage!, backgroundColor: Colors.red);
+    }
+  }
+  @override
+  void dispose() {
+    _emailTEController.dispose();
+    _passwordTEController.dispose();
+    super.dispose();
+  }
+
   void _onTapSignUpButton() {
     Navigator.pushNamed(context, SignUpScreen.routeName);
   }
